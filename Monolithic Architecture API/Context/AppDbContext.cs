55e900461a5175cdeff4b113b4ea5f36f2b3eb50
Lázaro <lazaro.19251953@gmail.com>
2026-06-21 11:17:49 -0300

    feat: create AppDbContext

---
+++ Monolithic Architecture API/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Monolithic_Architecture_API.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
