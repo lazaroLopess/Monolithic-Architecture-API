1e910821cd196a206029e698cd3cbe83f5fb1b47
Lázaro <lazaro.19251953@gmail.com>
2026-06-21 11:33:49 -0300

    feat: add Identity tables migration

--- Monolithic Architecture API/Migrations/20260621142931_InitialIdentitySchema.Designer.cs
+++ Monolithic Architecture API/Migrations/20260621142931_InitialIdentitySchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Monolithic_Architecture_API.Context;
 
@@ -11,9 +12,11 @@ using Monolithic_Architecture_API.Context;
 namespace Monolithic_Architecture_API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260621142931_InitialIdentitySchema")]
+    partial class InitialIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
