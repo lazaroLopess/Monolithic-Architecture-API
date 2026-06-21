b0f7ae890017969db4f7aea879764172b0a5c2b4
Lázaro <lazaro.19251953@gmail.com>
2026-06-21 12:25:09 -0300

    feat: update DbContext to support ApplicationUser

--- Monolithic Architecture API/Context/AppDbContext.cs
+++ Monolithic Architecture API/Context/AppDbContext.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Monolithic_Architecture_API.Identity;
 
 namespace Monolithic_Architecture_API.Context
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
