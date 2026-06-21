90514004d97123ab4b447a165f9869a5453a4b22
Lázaro <lazaro.19251953@gmail.com>
2026-06-21 10:56:46 -0300

    configure Swagger

--- Monolithic Architecture API/Program.cs
+++ Monolithic Architecture API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,33 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "testv1",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Lázaro", 
+            Email = "lazaro.19251953@gmail.com"
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "test");
+    });
 }
 
 app.UseHttpsRedirection();
