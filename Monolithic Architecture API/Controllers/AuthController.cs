a4cdf3020861bd871da2a603ccdc77c410f54c10
Lázaro <lazaro.19251953@gmail.com>
2026-06-21 12:21:37 -0300

    feat: add authentication controller

---
+++ Monolithic Architecture API/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Monolithic_Architecture_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("teste")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new
+            {
+                message = "teste rota 1"
+            });
+        }
+    }
+}
