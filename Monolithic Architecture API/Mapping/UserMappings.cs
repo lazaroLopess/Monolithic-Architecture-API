003868d131a0363b732c94bfe17695b3a584f1aa
Lázaro <lazaro.19251953@gmail.com>
2026-06-22 11:41:20 -0300

    feat: add user mapping extensions

---
+++ Monolithic Architecture API/Mapping/UserMappings.cs
@@ -0,0 +1,17 @@
+﻿using Monolithic_Architecture_API.DTOs.Requests;
+using Monolithic_Architecture_API.Identity;
+
+namespace Monolithic_Architecture_API.Mapping
+{
+    public static class UserMappings
+    {
+        public static ApplicationUser ToApplicationUser(this RegisterUserRequest request)
+        {
+            return new ApplicationUser
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+            };
+        }
+    }
+}
