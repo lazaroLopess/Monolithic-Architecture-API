0800e5ef94019a11c59976e00bcc0653b73d9e4a
Lázaro <lazaro.19251953@gmail.com>
2026-06-22 11:39:30 -0300

    feat: add RegisterUserRequest DTO

---
+++ Monolithic Architecture API/DTOs/Requests/RegisterUserRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monolithic_Architecture_API.DTOs.Requests
+{
+    public class RegisterUserRequest 
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MinLength(8, ErrorMessage = "The password must be at least 8 characters long.")]
+        public string Password {  get; set; } = string.Empty;
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
