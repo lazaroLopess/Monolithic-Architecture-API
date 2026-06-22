07742461b619c35daad462cca6dd78757d21dfa9
Lázaro <lazaro.19251953@gmail.com>
2026-06-22 20:46:37 -0300

    feat(auth): add RefreshTokenUserRequest with access and refresh tokens

---
+++ Monolithic Architecture API/DTOs/Requests/RefreshTokenUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monolithic_Architecture_API.DTOs.Requests
+{
+    public class RefreshTokenUserRequest
+    {
+        [Required]
+        public string AccessTokenExpired { get; set; } = string.Empty;
+        [Required]
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
