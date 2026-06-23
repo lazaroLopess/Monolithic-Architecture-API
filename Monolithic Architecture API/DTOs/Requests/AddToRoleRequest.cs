e6c5b2d3fab406ede1cdccdf2b95c8719f63b972
Lázaro <lazaro.19251953@gmail.com>
2026-06-23 12:13:23 -0300

    feat(auth): create request DTO for assigning roles to users

---
+++ Monolithic Architecture API/DTOs/Requests/AddToRoleRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monolithic_Architecture_API.DTOs.Requests
+{
+    public class AddToRoleRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Role { get; set; } = string.Empty;
+    }
+}
