3f749a05bdf2fc2ded53432c384c79d335575892
Lázaro <lazaro.19251953@gmail.com>
2026-06-23 20:34:51 -0300

    feat(users): add UserResponse DTO

---
+++ Monolithic Architecture API/DTOs/Responses/UserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Monolithic_Architecture_API.DTOs.Responses
+{
+    public class UserResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
