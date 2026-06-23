b08f2db578493e44e5850b7f25739a52a3b9b847
Lázaro <lazaro.19251953@gmail.com>
2026-06-23 20:34:24 -0300

    feat(users): add UpdateUserRequest DTO

---
+++ Monolithic Architecture API/DTOs/Requests/UpdateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monolithic_Architecture_API.DTOs.Requests
+{
+    public class UpdateUserRequest
+    {
+        [Required]
+        public string UserName {  get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
