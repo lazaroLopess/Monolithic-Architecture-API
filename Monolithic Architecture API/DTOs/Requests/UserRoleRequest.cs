3128bd8009001d3dd69af58af00690c192893b70
Lázaro <lazaro.19251953@gmail.com>
2026-06-24 09:25:01 -0300

    feat(roles): add UserRoleRequest DTO

---
+++ Monolithic Architecture API/DTOs/Requests/UserRoleRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monolithic_Architecture_API.DTOs.Requests
+{
+    public class UserRoleRequest
+    {
+        [Required]
+        public string Id { get; set; } = string.Empty;
+        [Required]
+        public string Role { get; set; } = string.Empty;
+    }
+}
