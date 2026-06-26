05fc2e97175f5bf0350c2b00340625041b9dfe87
Lázaro <lazaro.19251953@gmail.com>
2026-06-26 10:40:33 -0300

    feat(audit): create audit controller

---
+++ Monolithic Architecture API/Controllers/AuditController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Monolithic_Architecture_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuditController : ControllerBase
+    {
+
+    }
+}
