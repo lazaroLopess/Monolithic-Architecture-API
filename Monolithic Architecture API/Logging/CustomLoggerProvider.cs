a19c382cf1db03274ba49c064df8e743e88fdfc0
Lázaro <lazaro.19251953@gmail.com>
2026-06-26 10:38:28 -0300

    feat(logging): create custom logger provider

---
+++ Monolithic Architecture API/Logging/CustomLoggerProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Monolithic_Architecture_API.Logging
+{
+    public class CustomLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
