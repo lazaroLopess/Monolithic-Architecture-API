dae4c006110b407789c327f6c4ec90eb6669295d
Lázaro <lazaro.19251953@gmail.com>
2026-06-26 10:43:02 -0300

    feat(api): configure CORS for CRUD requests

--- Monolithic Architecture API/Program.cs
+++ Monolithic Architecture API/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Monolithic_Architecture_API.Context;
 using Monolithic_Architecture_API.Identity;
+using Monolithic_Architecture_API.Middleware;
 using Monolithic_Architecture_API.TokenServices;
 using System.Text;
 
@@ -80,6 +81,18 @@ builder.Services.AddAuthorization(options =>
     });
 });
 
+string corsPolicy_crud = "crudPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicy_crud, policy =>
+    {
+        policy
+        .WithOrigins("https://localhost:xxxx")
+        .WithMethods("GET", "POST", "PUT", "DELETE")
+        .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -91,11 +104,14 @@ if (app.Environment.IsDevelopment())
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "test");
     });
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseCors(corsPolicy_crud);
 
 app.UseAuthentication();
 
