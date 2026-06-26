d0327ce56e1669f5b4d9d783b0c4601553623bf5
Lázaro <lazaro.19251953@gmail.com>
2026-06-26 10:39:40 -0300

    feat(middleware): implement global exception handling middleware

---
+++ Monolithic Architecture API/Middleware/ApiExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Text.Json;
+
+namespace Monolithic_Architecture_API.Middleware
+{
+    public static class ApiExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if(contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(JsonSerializer.Serialize
+                        (
+                            new
+                            {
+                                context.Response.StatusCode,
+                                contextFeature.Error.Message,
+                                Trace = contextFeature.Error.StackTrace
+                            })
+                        );
+                    }
+                });
+            });
+        }
+    }
+}
